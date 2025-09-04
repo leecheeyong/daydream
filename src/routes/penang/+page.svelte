91c3bedd1fd3abc023318148590f2554ec7c7ae2
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2025-09-05 00:29:59 +0800

    Update +page.svelte

--- src/routes/penang/+page.svelte
+++ src/routes/penang/+page.svelte
@@ -1461,15 +1461,6 @@ Mumbai`.split("\n")
 		</div>
 		</div>
 
-		<!-- FAQ Item 2 -->
-		<div class="relative transform rotate-1">
-			<img src="window-4.png" alt="window" class="w-full h-full object-contain max-md:scale-130 max-xl:scale-110 max-lg:scale-115">
-			<div class="absolute top-20 left-12 right-12 bottom-16 flex flex-col items-center justify-center text-center px-24 opacity-70 max-[900px]:mx-[15vw] max-sm:mx-0 max-sm:px-5 max-lg:px-14 max-xl:px-18">
-				<h3 class="text-xl font-serif font-bold mb-4 max-lg:mb-0 max-md:text-base">Can I organize a Daydream in my city?</h3>
-				<p class="text-sm">Definitely! Contact us via daydream@hackclub.com or join #daydream on slack.</p>
-			</div>
-		</div>
-
 		<!-- FAQ Item 3 -->
 		<div class="relative transform rotate-2">
 			<img src="window-2.png" alt="window" class="w-full h-full object-contain max-md:scale-130 max-xl:scale-110 max-lg:scale-115">
